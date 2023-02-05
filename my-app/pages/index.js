bd990199556d9a33b31d0015554eafc70b7f3de2
omkarg01 <omkargujja01@gmail.com>
2023-02-05 23:27:08 +0530

    useEffect done

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -305,5 +305,38 @@ export default function Home() {
         return web3Provider;
     };
 
+    /*
+        connectWallet: Connects the MetaMask wallet
+      */const connectWallet = async () => {
+        try {
+            // Get the provider from web3Modal, which in our case is MetaMask
+            // When used for the first time, it prompts the user to connect their wallet
+            await getProviderOrSigner();
+            setWalletConnected(true);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
+    // useEffects are used to react to changes in state of the website
+    // The array at the end of function call represents what state changes will trigger this effect
+    // In this case, whenever the value of `walletConnected` changes - this effect will be called
+    useEffect(() => {
+        // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+        if (!walletConnected) {
+            // Assign the Web3Modal class to the reference object by setting it's `current` value
+            // The `current` value is persisted throughout as long as this page is open
+            web3ModalRef.current = new Web3Modal({
+                network: "goerli",
+                providerOptions: {},
+                disableInjectedProvider: false,
+            });
+
+            connectWallet();
+            getTotalTokensMinted();
+            getBalanceOfCryptoDevTokens();
+            getTokensToBeClaimed();
+            getOwner();
+        }
+    }, [walletConnected]);
 }
