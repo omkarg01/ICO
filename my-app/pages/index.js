4cfbbe73f3568970ddd3afe027afb685f819dc3b
omkarg01 <omkargujja01@gmail.com>
2023-02-05 23:25:13 +0530

    getProvider and Signer done

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -266,11 +266,44 @@ export default function Home() {
             await tx.wait();
             setLoading(false);
             await getOwner();
-            
+
         } catch (error) {
             console.error(err);
             window.alert(err.reason);
         }
     }
+
+    /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+    const getProviderOrSigner = async (needSigner = false) => {
+        // Connect to Metamask
+        // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+        const provider = await web3ModalRef.current.connect();
+        const web3Provider = new providers.Web3Provider(provider);
+
+        // If user is not connected to the Goerli network, let them know and throw an error
+        const { chainId } = await web3Provider.getNetwork();
+        if (chainId !== 5) {
+            window.alert("Change the network to Goerli");
+            throw new Error("Change network to Goerli");
+        }
+
+        if (needSigner) {
+            const signer = web3Provider.getSigner();
+            return signer;
+        }
+        return web3Provider;
+    };
+
 
 }
