e84851dc1c6b15f63a6f23a0dc817b1d0caf03d9
omkarg01 <omkargujja01@gmail.com>
2023-02-05 23:31:56 +0530

    index done

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -391,4 +391,58 @@ export default function Home() {
         );
     }
 
+    return (
+        <div>
+            <Head>
+                <title>Crypto Devs</title>
+                <meta name="description" content="ICO-Dapp" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.main}>
+                <div>
+                    <h1 className={styles.title}>Welcome to Crypto Devs ICO!</h1>
+                    <div className={styles.description}>
+                        You can claim or mint Crypto Dev tokens here
+                    </div>
+                    {walletConnected ? (
+                        <div>
+                            <div className={styles.description}>
+                                {/* Format Ether helps us in converting a BigNumber to string */}
+                                You have minted {utils.formatEther(balanceOfCryptoDevTokens)} Crypto
+                                Dev Tokens
+                            </div>
+                            <div className={styles.description}>
+                                {/* Format Ether helps us in converting a BigNumber to string */}
+                                Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+                            </div>
+                            {renderButton()}
+                            {/* Display additional withdraw button if connected wallet is owner */}
+                            {isOwner ? (
+                                <div>
+                                    {loading ? <button className={styles.button}>Loading...</button>
+                                        : <button className={styles.button} onClick={withdrawCoins}>
+                                            Withdraw Coins
+                                        </button>
+                                    }
+                                </div>
+                            ) : ("")
+                            }
+                        </div>
+                    ) : (
+                        <button onClick={connectWallet} className={styles.button}>
+                            Connect your wallet
+                        </button>
+                    )}
+                </div>
+                <div>
+                    <img className={styles.image} src="./0.svg" />
+                </div>
+            </div>
+
+            <footer className={styles.footer}>
+                Made with &#10084; by Crypto Devs
+            </footer>
+        </div>
+    )
+
 }
